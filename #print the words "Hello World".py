0c24c7f9e15a520af0aa63329815a7dbbd54d1b6
Ralvey1 <102349808+Ralvey1@users.noreply.github.com>
2022-06-11 13:43:48 -0400

    Commit to main

---
+++ #print the words "Hello World".py
@@ -0,0 +1,26 @@
+#print the words "Hello World"
+print("Hello World")
+#this is a list with multiple valiues
+list = [1, 2, 3, 4, 5, 6]
+#this is appending a vaule to a list
+list.append(7)
+list.append(8)
+list.extend([9,10,11,12,13])
+print(5)
+#this is 2 list with several values
+a = [1,2,3,4]
+b = [5,6,7,8]
+letter =  ["a","b","c","d"]
+letter.extend(["e","f","g","h"])
+print(letter)
+a , b = 8, 4
+print(a)
+print(b)
+frt = { "apple" : .89 ,
+       "orange" : .79 ,
+       "pineapple" : 3.50 , 
+       "strawberry" : .45 
+      }
+print(frt.values())
+tpl = ( '1', '2', '3', '4')
+print(tpl[2])
